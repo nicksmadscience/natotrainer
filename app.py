beadac21be2695356a5c71de91714a24213c2c0c
nicksmadscience <nickpiegari@gmail.com>
2026-02-02 18:20:32 -0500

    embed style tweaks

--- app.py
+++ app.py
@@ -54,4 +54,4 @@ if __name__ == "__main__":
 	args = parser.parse_args()
 
 	if args.mode == "run":
-		app.run(debug=True, host="0.0.0.0", port=5001)
+		app.run(debug=True, host="0.0.0.0", port=5002)
